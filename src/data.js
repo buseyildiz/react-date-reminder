1421cba32e93dc1ca0efd88dca54c8304aa3ea5f
Buse Yıldız <102406546+buseyildiz@users.noreply.github.com>
2023-01-27 15:19:09 +0300

    dates-reminder

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default[
+    {
+        id:1,
+        title:"Ulusal Egemenlik ve Çocuk Bayramı",
+        img:"https://images.gonulluyuzbiz.gov.tr/public/39475.jpg?width=1200&height=800&mode=max",
+        date:"23 Nisan"
+    },
+    {
+        id:2,
+        title:"Zafer Bayramı",
+        img:"https://imgs.stargazete.com/imgsdisk/2022/08/29/30-agustos-zafer-bayrami--403_2-41.jpg",
+        date:"30 Ağustos"
+    },
+    {
+        id:3,
+        title:"Atatürkü Anma ,Gençlik ve Spor Bayramı",
+        img:"https://i4.hurimg.com/i/hurriyet/75/750x422/627d9d8367b0a947ccf18315.jpg",
+        date:"19 Mayıs"
+    },
+    {
+        id:4,
+        title:"Cumhuriyer Bayramı",
+        img:"https://img.memurlar.net/galeri/21305/60a896d5-4738-ec11-8105-a0369f7d1484.png?width=800",
+        date:"29 Ekim"
+    }
+]
